7d3367cbc06cc9670f70f530821de4ada076ed4f
Milan Latinović <milan.softline@gmail.com>
2016-11-29 15:11:57 +0100

    HelloWorld primjer zavrsen

---
+++ Primjer01 - HelloWorld/src/App.java
@@ -0,0 +1,19 @@
+/**
+ * Ovo je primjer App klase 
+ * 
+ * @author milan.latinovic
+ *
+ */
+public class App {
+
+	/**
+	 * @param args - primjer komentara na funkciju
+	 */
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
