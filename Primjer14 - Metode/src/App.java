4979c3d6e175a1945cb901251f04e8d677e7d66d
Milan Latinović <milan.softline@gmail.com>
2016-12-13 15:09:58 +0100

    Primjer nasljedjivanja sa komentarom

--- Primjer14 - Metode/src/App.java
+++ Primjer14 - Metode/src/App.java
@@ -1,8 +1,16 @@
+// Klasa Person...
 class Person {
  
+	// Klasa ima dva atributa, ime i godine
     String name;
     int age;
      
+    // Klasa ima vise funkcija
+    
+    /*
+     * Funkcija speak() ima parametar void 
+     * void - ne vraca vrijednost
+     */
     void speak() {
         for(int i=0; i<3; i++) {
             System.out.println("Moje ime je: " + name + " i ja imam " + age + " godina. ");
@@ -13,11 +21,44 @@ class Person {
         System.out.println("Pozdrav svima!");
     }
 }
+
+/*
+ * Primjeri nasljedjivanja
+class Student extends Person {
+	String index;
+}
+
+class Profesor extends Person {
+	String radnaKnjizica;
+	boolean zaposlen;
+}
+
+class Izvjestaj {
+ int id;
+ String datum;
+}
+
+class IzvjestajSaIspita extends Izvjestaj {
+	int ocjena;
+	Student student;
+	Profesor profesor;
+}
+*/
+
+
+
  
 public class App {
  
     public static void main(String[] args) {
- 
+    	/*
+    	 * Primjeri koji sljede
+    	IzvjestajSaIspita izvjestaj = new IzvjestajSaIspita();
+    	izvjestaj.profesor.name = "Goran";
+    	izvjestaj.student.name = "Marko";
+    	izvjestaj.datum = "21.01.2016";
+    	izvjestaj.ocjena = 7;
+    	 */
         Person person1 = new Person();
         person1.name = "Marko Javic";
         person1.age = 37;
@@ -28,7 +69,7 @@ public class App {
         person2.name = "Goran Eclipsic";
         person2.age = 20;
         person2.speak();
-        person1.sayHello();
+        person2.sayHello();
  
         System.out.println(person1.name);
  
