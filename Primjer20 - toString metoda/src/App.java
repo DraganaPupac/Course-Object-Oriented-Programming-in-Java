60802ba7ae8bd36ca36501064916067c8e601b30
Milan Latinović <milan.softline@gmail.com>
2016-12-14 12:16:41 +0100

    Kompletna promjena primjera, automatsko generisanje koda

--- Primjer20 - toString metoda/src/App.java
+++ Primjer20 - toString metoda/src/App.java
@@ -1,33 +1,249 @@
 class User {
      
-    private int id;
-    private String name;
+     private int id;
+     private String firstName;
+     private String lastName;
+     private String accountType;
+     private String gender;
+     private String dateOfBirth;
+     private int age;
+     private String icon;
+     private String banknumber;
      
-    public User(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public User(int id, String firstName, String lastName, String accountType, String gender, String dateOfBirth,
+			int age, String icon, String banknumber) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountType = accountType;
+		this.gender = gender;
+		this.dateOfBirth = dateOfBirth;
+		this.age = age;
+		this.icon = icon;
+		this.banknumber = banknumber;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the accountType
+	 */
+	public String getAccountType() {
+		return accountType;
+	}
+
+	/**
+	 * @param accountType the accountType to set
+	 */
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the dateOfBirth
+	 */
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 */
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the icon
+	 */
+	public String getIcon() {
+		return icon;
+	}
+
+	/**
+	 * @param icon the icon to set
+	 */
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	/**
+	 * @return the banknumber
+	 */
+	public String getBanknumber() {
+		return banknumber;
+	}
+
+	/**
+	 * @param banknumber the banknumber to set
+	 */
+	public void setBanknumber(String banknumber) {
+		this.banknumber = banknumber;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", accountType=" + accountType
+				+ ", gender=" + gender + ", dateOfBirth=" + dateOfBirth + ", age=" + age + ", icon=" + icon
+				+ ", banknumber=" + banknumber + "]";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((accountType == null) ? 0 : accountType.hashCode());
+		result = prime * result + age;
+		result = prime * result + ((banknumber == null) ? 0 : banknumber.hashCode());
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((icon == null) ? 0 : icon.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (accountType == null) {
+			if (other.accountType != null)
+				return false;
+		} else if (!accountType.equals(other.accountType))
+			return false;
+		if (age != other.age)
+			return false;
+		if (banknumber == null) {
+			if (other.banknumber != null)
+				return false;
+		} else if (!banknumber.equals(other.banknumber))
+			return false;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (icon == null) {
+			if (other.icon != null)
+				return false;
+		} else if (!icon.equals(other.icon))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
      
-    public String toString() {
-         
-        return String.format("%-4d: %s", id, name);
-         
-        /*
-        StringBuilder sb = new StringBuilder();
-        sb.append(id).append(": ").append(name);
-         
-        return sb.toString();
-        */
-    }
+     
+     
+     
 }
  
 public class App {
  
     public static void main(String[] args) {
-        User user1 = new User(7, "Marko");
-        User user2 = new User(5, "Goran");
-         
-        System.out.println(user1);
-        System.out.println(user2);
+    	
+    	User korisnik1 = new User(3, "Marko", "Markovic", "juzer", "m", "11.11.1980.", 36, "http://www.nekisajt.com/bin/neki.ico", "123123123132132");
+    	
+    	System.out.println(korisnik1.toString());
     }
 }
