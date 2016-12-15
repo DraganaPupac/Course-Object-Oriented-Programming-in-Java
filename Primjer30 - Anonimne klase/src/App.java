e5cb56fbcf68bab82914deb42b0a3a2ec0024794
Milan Latinović <milan.softline@gmail.com>
2016-12-15 12:30:20 +0100

    Izbrisani komentari sa Anonimnih klasa

--- Primjer30 - Anonimne klase/src/App.java
+++ Primjer30 - Anonimne klase/src/App.java
@@ -11,23 +11,15 @@ interface Plant {
 public class App {
  
     public static void main(String[] args) {
-         
-    	/*
-    	 * Kreiramo anonimnu klasu i mjenjamo funkcionalnost start metode
-    	 * Anonimna klasa je "naslonjena" na promjenjivu machine1
-    	 */
+
         Machine machine1 = new Machine() {
             @Override public void start() {
                 System.out.println("Kamera slika ....");
             }
         };
-         
+
         machine1.start();
         
-        /*
-         * Kreiramo anonimnu klasu...
-         * Ovo je ekvivalentno situaciji kada implementiramo Plant interfejs
-         */
         Plant plant1 = new Plant() {
             @Override
             public void grow() {
